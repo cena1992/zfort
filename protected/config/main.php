9152a6f53d91fabb42a09107e6ec40fc88334657
Игорь <@109.87.98.2>
2013-04-14 17:40:25 +0300

    1.Сделал модуль админ и для него меню

--- protected/config/main.php
+++ protected/config/main.php
@@ -27,6 +27,11 @@ return array(
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
+            'admin'=>array(
+            'modules'=>array(
+        'admin',
+    ),
+                ),
 		
 	),
 
@@ -39,11 +44,13 @@ return array(
 		// uncomment the following to enable URLs in path-format
 		
 		'urlManager'=>array(
+                    
 			'urlFormat'=>'path',
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+                                
 			),
 		),
 		/*
--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -5,8 +5,10 @@ class SiteController extends Controller
 	/**
 	 * Declares class-based actions.
 	 */
+       
 	public function actions()
 	{
+            
 		return array(
 			// captcha action renders the CAPTCHA image displayed on the contact page
 			'captcha'=>array(
@@ -25,10 +27,12 @@ class SiteController extends Controller
 	 * This is the default 'index' action that is invoked
 	 * when an action is not explicitly requested by users.
 	 */
+        
 	public function actionIndex()
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
+          
 		$this->render('index');
 	}
 
